b07e6442bdab306719d16eec4eb657bee0cb9627
Ycany a <ycanyashra.sanchez2006@gmail.com>
2026-02-06 13:51:54 +0800

    Delete Map Coding directory

--- Map Coding/sidebar.js
+++
@@ -1,6 +0,0 @@
-const closeButton = document.querySelector(".js-close-icon-btn");
-const sidebar = document.querySelector(".js-sidebar");
-
-closeButton.addEventListener("click", () => {
-  sidebar.style.display = "none";
-});
